fd61629b81f711df92faaeb611e012bb8d95d2bc
WCEdmonds <wcedmonds28@gmail.com>
2015-09-20 10:34:35 -0400

    Much better!

--- repeater.rb
+++ repeater.rb
@@ -3,18 +3,19 @@ require "pry"
 #write a method that accepts two paramters:
   # a string to be repeatedly put to the string
   # how many times that string should be repeated
-def repeat(input, n)
-  binding.pry
-  puts input * n
+def repeat(string, number_of_times)
+  number_of_times.times do
+    puts string
+  end
 end
 # ask the user for a string
 puts "Pick your favorite word!"
 # store the users input in a variable
-input = gets
+string = gets.strip
 # ask the user for the number of times to repeat the string
 puts "Give me a number!"
 # store the users input in a variable
-n = gets.to_i
+number_of_times = gets.strip
 # call the method
 
-repeat(input, n)
+repeat(string, number_of_times)
